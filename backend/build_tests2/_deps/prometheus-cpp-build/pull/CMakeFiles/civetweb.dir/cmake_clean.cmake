3f4929fd3ad3d3ea434887696759b15376b22a9c
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:04:16 +0800

    Update project

---
+++ backend/build_tests2/_deps/prometheus-cpp-build/pull/CMakeFiles/civetweb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/civetweb.dir/__/3rdparty/civetweb/src/CivetServer.cpp.obj"
+  "CMakeFiles/civetweb.dir/__/3rdparty/civetweb/src/CivetServer.cpp.obj.d"
+  "CMakeFiles/civetweb.dir/__/3rdparty/civetweb/src/civetweb.c.obj"
+  "CMakeFiles/civetweb.dir/__/3rdparty/civetweb/src/civetweb.c.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/civetweb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
