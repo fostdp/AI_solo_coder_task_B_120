2fd8903e92040174bd923c56ab977a6c0ed6c7f8
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:04:35 +0800

    Update project

---
+++ backend/build_tests2/_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/build_tests2/_deps/prometheus-cpp-src/pull/src/basic_auth.cc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/basic_auth.cc.obj" "gcc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/basic_auth.cc.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/build_tests2/_deps/prometheus-cpp-src/pull/src/endpoint.cc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/endpoint.cc.obj" "gcc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/endpoint.cc.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/build_tests2/_deps/prometheus-cpp-src/pull/src/exposer.cc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/exposer.cc.obj" "gcc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/exposer.cc.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/build_tests2/_deps/prometheus-cpp-src/pull/src/handler.cc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/handler.cc.obj" "gcc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/handler.cc.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/build_tests2/_deps/prometheus-cpp-src/pull/src/metrics_collector.cc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/metrics_collector.cc.obj" "gcc" "_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/src/metrics_collector.cc.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
