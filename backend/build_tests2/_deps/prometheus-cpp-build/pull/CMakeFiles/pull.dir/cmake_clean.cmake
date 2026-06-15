d2f24b6a868e237176565a4dc5268f31d6c58d72
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:04:29 +0800

    Update project

---
+++ backend/build_tests2/_deps/prometheus-cpp-build/pull/CMakeFiles/pull.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../lib/libprometheus-cpp-pull.a"
+  "../lib/libprometheus-cpp-pull.pdb"
+  "CMakeFiles/pull.dir/src/basic_auth.cc.obj"
+  "CMakeFiles/pull.dir/src/basic_auth.cc.obj.d"
+  "CMakeFiles/pull.dir/src/endpoint.cc.obj"
+  "CMakeFiles/pull.dir/src/endpoint.cc.obj.d"
+  "CMakeFiles/pull.dir/src/exposer.cc.obj"
+  "CMakeFiles/pull.dir/src/exposer.cc.obj.d"
+  "CMakeFiles/pull.dir/src/handler.cc.obj"
+  "CMakeFiles/pull.dir/src/handler.cc.obj.d"
+  "CMakeFiles/pull.dir/src/metrics_collector.cc.obj"
+  "CMakeFiles/pull.dir/src/metrics_collector.cc.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/pull.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
