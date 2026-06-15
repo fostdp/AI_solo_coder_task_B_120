93158b84115399caa7cdd68efa72617c198a9f71
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:02:51 +0800

    Update project

---
+++ backend/build_tests2/CMakeFiles/crossbow_backend_tests.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/bowstring_material.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/bowstring_material.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/bowstring_material.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/dynamics_model.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/dynamics_model.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/dynamics_model.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/formation_simulator.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/formation_simulator.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/formation_simulator.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/logger.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/logger.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/logger.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/metrics.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/metrics.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/metrics.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/src/sight_optics.cpp" "CMakeFiles/crossbow_backend_tests.dir/src/sight_optics.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/src/sight_optics.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/tests/test_bowstring.cpp" "CMakeFiles/crossbow_backend_tests.dir/tests/test_bowstring.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/tests/test_bowstring.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/tests/test_formation.cpp" "CMakeFiles/crossbow_backend_tests.dir/tests/test_formation.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/tests/test_formation.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/tests/test_main.cpp" "CMakeFiles/crossbow_backend_tests.dir/tests/test_main.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/tests/test_main.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/tests/test_shooting.cpp" "CMakeFiles/crossbow_backend_tests.dir/tests/test_shooting.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/tests/test_shooting.cpp.obj.d"
+  "D:/SOLO-2/AI_solo_coder_task_B_120/backend/tests/test_sight_optics.cpp" "CMakeFiles/crossbow_backend_tests.dir/tests/test_sight_optics.cpp.obj" "gcc" "CMakeFiles/crossbow_backend_tests.dir/tests/test_sight_optics.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
