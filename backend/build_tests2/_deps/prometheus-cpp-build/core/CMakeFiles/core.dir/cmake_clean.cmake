7cd7812ad0e879e41e604682a7290ef4c7d51ee1
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:03:57 +0800

    Update project

---
+++ backend/build_tests2/_deps/prometheus-cpp-build/core/CMakeFiles/core.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "../lib/libprometheus-cpp-core.a"
+  "../lib/libprometheus-cpp-core.pdb"
+  "CMakeFiles/core.dir/src/check_names.cc.obj"
+  "CMakeFiles/core.dir/src/check_names.cc.obj.d"
+  "CMakeFiles/core.dir/src/counter.cc.obj"
+  "CMakeFiles/core.dir/src/counter.cc.obj.d"
+  "CMakeFiles/core.dir/src/detail/builder.cc.obj"
+  "CMakeFiles/core.dir/src/detail/builder.cc.obj.d"
+  "CMakeFiles/core.dir/src/detail/ckms_quantiles.cc.obj"
+  "CMakeFiles/core.dir/src/detail/ckms_quantiles.cc.obj.d"
+  "CMakeFiles/core.dir/src/detail/time_window_quantiles.cc.obj"
+  "CMakeFiles/core.dir/src/detail/time_window_quantiles.cc.obj.d"
+  "CMakeFiles/core.dir/src/detail/utils.cc.obj"
+  "CMakeFiles/core.dir/src/detail/utils.cc.obj.d"
+  "CMakeFiles/core.dir/src/family.cc.obj"
+  "CMakeFiles/core.dir/src/family.cc.obj.d"
+  "CMakeFiles/core.dir/src/gauge.cc.obj"
+  "CMakeFiles/core.dir/src/gauge.cc.obj.d"
+  "CMakeFiles/core.dir/src/histogram.cc.obj"
+  "CMakeFiles/core.dir/src/histogram.cc.obj.d"
+  "CMakeFiles/core.dir/src/info.cc.obj"
+  "CMakeFiles/core.dir/src/info.cc.obj.d"
+  "CMakeFiles/core.dir/src/registry.cc.obj"
+  "CMakeFiles/core.dir/src/registry.cc.obj.d"
+  "CMakeFiles/core.dir/src/serializer.cc.obj"
+  "CMakeFiles/core.dir/src/serializer.cc.obj.d"
+  "CMakeFiles/core.dir/src/summary.cc.obj"
+  "CMakeFiles/core.dir/src/summary.cc.obj.d"
+  "CMakeFiles/core.dir/src/text_serializer.cc.obj"
+  "CMakeFiles/core.dir/src/text_serializer.cc.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
